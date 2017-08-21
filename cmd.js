25dc6debc706c291948019ea8abd1f234eb9a8b8
Bradley Scott <bscott28@une.edu.au>
2017-08-21 15:31:47 +1000

    Refactored code.  Put the models into the model directory

--- cmd.js
+++ cmd.js
@@ -9,6 +9,9 @@
 var moment = require('moment');
 var logger = require('./logger');
 var utils = require('crc-utils');
+var Task = require('./model/Task');
+var TaskCollection = require('./model/TaskCollection');
+
 /**
  * Set the log level
  * Defaults to 'error'
@@ -24,39 +27,6 @@ logger.info('Running command: ', {
     loopCnt: loopCnt
 });
 
-var Task = function (data) {
-    this.name = data.name;
-    this.priority = data.priority;
-    this.project = data.project;
-    this.user = data.user;
-    this.completed = data.completed;
-};
-
-Task.prototype.getPriority = function () {
-    return this.priority;
-};
-
-function TaskCollection() {
-    var tasks = {};
-    var count = 0;
-    var add = function (data) {
-        tasks[data.name] =
-            new Task(data);
-        count++;
-    };
-    var get = function (name) {
-        return tasks[name];
-    };
-    var getCount = function () {
-        return count;
-    };
-    return {
-        add: add,
-        get: get,
-        getCount: getCount
-    };
-}
-
 var tasks = new TaskCollection();
 
 var projects = ['none', 'courses', 'training', 'project'];
@@ -64,23 +34,38 @@ var priorities = [1, 2, 3, 4, 5];
 var users = ['Jon', 'Erica', 'Amanda', 'Nathan'];
 var completed = [true, false];
 
-var initialMemory = process.memoryUsage().heapUsed;
-var hrStart = process.hrtime();
+function execute() {
 
-for (var i = 0; i < loopCnt; i++) {
-    tasks.add({
-        name: 'task' + i,
-        priority: priorities[Math.floor((Math.random() * 5))],
-        project: projects[Math.floor((Math.random() * 4))],
-        user: users[Math.floor((Math.random() * 4))],
-        completed: completed[Math.floor((Math.random() * 2))]
-    });
+    var initialMemory = process.memoryUsage().heapUsed;
+    var hrStart = process.hrtime();
 
-};
+    for (var i = 0; i < loopCnt; i++) {
+        tasks.add({
+            name: 'task' + i,
+            priority: priorities[Math.floor((Math.random() * 5))],
+            project: projects[Math.floor((Math.random() * 4))],
+            user: users[Math.floor((Math.random() * 4))],
+            completed: completed[Math.floor((Math.random() * 2))]
+        });
 
-var afterMemory = process.memoryUsage().heapUsed;
-var end = process.hrtime(hrStart);
+    };
 
-logger.info('used memory ' + (afterMemory - initialMemory) / 1000000);
-logger.info('timetake: ', end[1] / 1000000);
-logger.info("tasks: " + tasks.getCount());
+    var afterMemory = process.memoryUsage().heapUsed;
+    var end = process.hrtime(hrStart);
+
+    var returnObj = {
+        pid: process.pid,
+        memUsed: (afterMemory - initialMemory) / 1000000,
+        timeTake: end[1] / 1000000,
+        tasks: tasks.getCount()
+    };
+    return returnObj;
+}
+
+process.on('message', function (msg) {
+    var signal = JSON.parse(msg);
+    logger.verbose('on message: ', signal);
+    var results = execute();
+    results.origMsg = signal;
+    process.send(JSON.stringify(results));
+});
